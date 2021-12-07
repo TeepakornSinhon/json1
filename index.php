12f2cd258ee778400d0c04f59b4801d15f5a0010
TeepakornSinhon <86229433+TeepakornSinhon@users.noreply.github.com>
2021-12-07 11:40:39 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,30 +8,42 @@
     <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
 </head>
 <body>
-    <button id="btnJSON1">JSON 1 </button>
-    name:<span id="fname"></span> <br/>
-    Sname:<span id="lname"></span>
+ 
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th><th>First Name</th><th>Last Name</th>
+            </tr>
+        </thead>
+        <tbody id="tblStudent">
+        </tbody>
+    </table>
 </body>
-
 <script>
+ 
 function loadJSON(){
-    var url="https://cj-android-demon.herokuapp.com/json2.php";
-    
+    var url = "https://cj-android-demon.herokuapp.com/json2.php";
+
     $.getJSON(url)
         .done((data)=>{
             console.log(data);
-            console.log(data[1]);
-            console.log(data[2].fname);
-            // $("#fname").text(data.fname);
+            $.each(data, (k, item)=>{
+                console.log(k);
+                console.log(item);
+                var line = "<tr>";
+                    line = "<td>" + (k+1) +"</td>"
+                    line = "<td>" + item.fname +"</td>"
+                    line = "<td>" + item.lname +"</td>"
+                $("#tblData").append(line);
+            })
         })
         .fail((xhr, status, err)=>{
-            
-        });
 
+        });
 }
 
 $(()=>{
-    $("#btnJSON1").click(loadJSON);
+    loadJSON();
 });
 </script>
 </html>
