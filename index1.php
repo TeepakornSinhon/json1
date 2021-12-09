8d91e6b36df6e64b342564dae6383d80e3892ebc
TeepakornSinhon <86229433+TeepakornSinhon@users.noreply.github.com>
2021-12-09 13:57:44 +0700

    Create index1.php

---
+++ index1.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Hello, world!</title>
+</head>
+<style>
+    .bg1 {
+        background-color: cadetblue;
+    }
+    .bg2 {
+        background-color: khaki;
+    }
+    .bg3 {
+        background-color: skyblue;
+        width: 200px;
+        height: 800px;
+        float: left;
+    }
+    .bg4 {
+        background-color: thistle;
+    }
+</style>
+
+<body>
+    <div class="container">
+        <img src="https://img.pptvhd36.com/thumbor/2021/09/14/news-4b19c7b231.jpg"class="img-fluid" style= "max-width: 100%;height: auto;">  
+        <div class="col bg3"></div>
+
+        <div class="row">
+            <div class="col-6">
+                <div class="card" style="width: 350px;height: 500px;">
+                    <img src="https://upload.wikimedia.org/wikipedia/th/thumb/c/cc/Chelsea_FC.svg/1200px-Chelsea_FC.svg.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">สโมสรฟุตบอลเชลซี (Chelsea Football Club) เป็นสโมสรฟุตบอลอาชีพที่ตั้งอยู่ในเขตฟูลัม, ลอนดอน ซึ่งเล่นอยู่ในพรีเมียร์ลีก ลีกสูงสุดของฟุตบอลอังกฤษ ก่อตั้งขึ้นใน ค.ศ. 1905 มีสนามเหย้าคือสแตมฟอร์ดบริดจ์</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6">
+                <div class="card" style="width: 350px; height: 500px;">
+                    <img src="https://img.pptvhd36.com/thumbor/2021/02/24/news-2f8e981098.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">โทมัส ทุคเคิล (Thomas Tuchel) เกิด 29 สิงหาคม ค.ศ. 1973 เป็นผู้ฝึกสอนและอดีตผู้เล่นชาวเยอรมัน ปัจจุบันเป็นหัวหน้าผู้ฝึกสอนให้แก่เชลซี</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12">
+                <div class="card" style="width: auto; height: 300px;"> 
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12 bg4" >
+                    <p>63103949 ทีปกร สินธุ์อ่อน</p>
+            </div>
+        </div>
+    </body>
+
+</html>
